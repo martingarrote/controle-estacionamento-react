7de14dc34132b48b93820665070ce6d81ad35181
martingarrote <martindambrowskigarrote@gmail.com>
2025-05-11 15:37:11 -0300

    implementar visualização em lista e melhoras na responsividade

--- src/pages/ReservationsListPage.js
+++ src/pages/ReservationsListPage.js
@@ -2,18 +2,37 @@ import './ReservationsListPage.css';
 
 import Table from "../components/Table";
 import { useParkingContext } from "../context/ParkingContext";
+import { useState } from 'react';
+import List from '../components/List';
 
 const ReservationsListPage = () => {
 
+    const [isListView, setIsListView] = useState(false);
     const { reservations } = useParkingContext();
+
+    const toggleView = () => {
+        setIsListView(!isListView)
+    }
 
     return (
         <section>
             <h2>Reservas</h2>
             <p>Listagem das reservas realizadas</p>
-            <div id="res-table-container">
-                <Table data={reservations}/>
-            </div>
+            <br/>
+
+            <button id="view-toggle-button" onClick={toggleView}>
+                {isListView ? 'Visualização em Tabela' : 'Visualização em Lista'}
+            </button>
+
+            {isListView ? (
+                <div id="res-list-container">
+                    <List data={reservations} />
+                </div>
+            ) : (
+                <div id="res-table-container">
+                    <Table data={reservations} />
+                </div>
+            )}
         </section>
     )
 }
