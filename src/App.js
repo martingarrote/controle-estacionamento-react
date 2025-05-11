263db748206bf31d44b5201ff2a4eecd91614b88
martingarrote <martindambrowskigarrote@gmail.com>
2025-05-11 01:25:39 -0300

    adicionar roteamento

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { ParkingProvider } from "./context/ParkingContext";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import CreateReservationPage from "./pages/CreateReservationPage";
+import ReservationsListPage from "./pages/ReservationsListPage";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" exact/>
-      </Routes>
-    </Router>
-  );
+    return (
+        <ParkingProvider>
+            <Router>
+                <Header/>
+                    <main>
+                        <Routes>
+                            
+                            <Route path='/vagas' exact Component={HomePage} />
+                            <Route path='/vagas/reservar/:spotNumber' exact Component={CreateReservationPage} />
+                            <Route path='/reservas' exact Component={ReservationsListPage} />
+
+                        </Routes>
+                    </main>
+            </Router>
+        </ParkingProvider>
+    )
 }
 
 export default App;
