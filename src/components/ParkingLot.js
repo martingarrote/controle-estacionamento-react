b865b67263c2b19ffc1cff848eb4155ba9d68816
martingarrote <martindambrowskigarrote@gmail.com>
2025-05-11 01:08:45 -0300

    adicionar ParkingLot para renderização e interação com vagas

---
+++ src/components/ParkingLot.js
@@ -0,0 +1,32 @@
+import ParkingSpot from "./ParkingSpot";
+import { useNavigate } from 'react-router-dom';
+import { useParkingContext } from '../context/ParkingContext';
+
+const ParkingLot = () => {
+
+    const { spots } = useParkingContext();
+    const navigate = useNavigate();
+
+    const handleRentSpot = (number) => {
+        const spot = spots.find(spot => spot.number === number);
+
+        if (spot && spot.status === 'free') {
+            navigate(`/reservas/${number}`);
+        }
+    };
+
+    return (
+        <div className="parking-lot">
+            {spots.map(spot => (
+                <ParkingSpot
+                    key={spot.number}
+                    number={spot.number}
+                    status={spot.status}
+                    onRent={handleRentSpot}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default ParkingLot
