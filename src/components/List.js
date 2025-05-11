34c4e5857ab58410daa6e861a4f1455de941d494
martingarrote <martindambrowskigarrote@gmail.com>
2025-05-11 15:36:35 -0300

    melhorar componente list para retorno visual quando não houverem reservas

--- src/components/List.js
+++ src/components/List.js
@@ -1,40 +1,49 @@
 import React from "react";
 
-const List = ({data}) => {
+const List = ({ data }) => {
     return (
-        data.map((reservation) => (
-            <div className="list-item" key={reservation.parkingSpotNumber}>
-                <div className="list-row">
-                    <strong>Número da vaga:</strong>
-                    <span>{reservation.parkingSpotNumber}</span>
+        <div>
+            {data.length === 0 ? (
+                <div className="list-item" style={{textAlign: "center"}}>
+                   Nenhum item encontrado.
                 </div>
-                <div className="list-row">
-                    <strong>Nome do Proprietário:</strong>
-                    <span>{reservation.ownerName}</span>
-                </div>
-                <div className="list-row">
-                    <strong>Número do Apartamento:</strong>
-                    <span>{reservation.apartmentNumber}</span>
-                </div>
-                <div className="list-row">
-                    <strong>Bloco do Apartamento:</strong>
-                    <span>{reservation.apartmentBlock}</span>
-                </div>
-                <div className="list-row">
-                    <strong>Modelo do Veículo:</strong>
-                    <span>{reservation.vehicleModel}</span>
-                </div>
-                <div className="list-row">
-                    <strong>Placa do Veículo:</strong>
-                    <span>{reservation.vehicleSign}</span>
-                </div>
-                <div className="list-row">
-                    <strong>Cor do Veículo:</strong>
-                    <span>{reservation.vehicleColor}</span>
-                </div>
-            </div>
-        ))
-    )
-}
+            ) : (
+                data.map((reservation) => (
+                    <div className="list-item" key={reservation.parkingSpotNumber}>
+                        <div className="list-row">
+                            <strong>Número da vaga:</strong>
+                            <span>{reservation.parkingSpotNumber}</span>
+                        </div>
+                        <div className="list-row">
+                            <strong>Nome do Proprietário:</strong>
+                            <span>{reservation.ownerName}</span>
+                        </div>
+                        <div className="list-row">
+                            <strong>Número do Apartamento:</strong>
+                            <span>{reservation.apartmentNumber}</span>
+                        </div>
+                        <div className="list-row">
+                            <strong>Bloco do Apartamento:</strong>
+                            <span>{reservation.apartmentBlock}</span>
+                        </div>
+                        <div className="list-row">
+                            <strong>Modelo do Veículo:</strong>
+                            <span>{reservation.vehicleModel}</span>
+                        </div>
+                        <div className="list-row">
+                            <strong>Placa do Veículo:</strong>
+                            <span>{reservation.vehicleSign}</span>
+                        </div>
+                        <div className="list-row">
+                            <strong>Cor do Veículo:</strong>
+                            <span>{reservation.vehicleColor}</span>
+                        </div>
+                    </div>
+                ))
+            )}
+        </div>
+    );
+};
+
 
 export default List;
